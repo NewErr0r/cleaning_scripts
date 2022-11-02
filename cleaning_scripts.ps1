437d96cc7b64bdefb45bee028a58731f9e1dbbba
Dima <sechmma@gmail.com>
2022-11-02 23:24:07 +0300

    first commit

---
+++ cleaning_scripts.ps1
@@ -0,0 +1,67 @@
+# явно прописаны пути к основным известным временным каталогам, есть возможность добавить в эту строку пути к каким-то СВОИМ каталогам, которые также требуют периодической очистки
+$OtherCleanPathsArr = 'C:\Temp\*', 'C:\Windows\Temp\*', 'C:\swsetup'
+
+#системные пути для очистки
+$InProfilesCleanPathsArr = '\AppData\Local\Temp\*', '\AppData\Local\*.auc', '\AppData\Local\Microsoft\Terminal Server Client\Cache\*', '\AppData\Local\Microsoft\Windows\Temporary Internet Files\*', '\AppData\Local\Microsoft\Windows\WER\ReportQueue\*', '\AppData\Local\Microsoft\Windows\Explorer\*'
+
+#пути в профилях для очистки
+$Profiles = Get-ChildItem (Get-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList').ProfilesDirectory -Exclude 'Administrator', 'Setup', 'Public', 'All Users', 'Default User'
+#извлекли из реестра местоположение профилей, сформировали список
+
+ForEach ($Path in $OtherCleanPathsArr) {
+Remove-Item -Path $Path -Recurse -Force -ErrorAction SilentlyContinue
+}
+ForEach ($Profile in $Profiles) {
+ForEach ($Path in $InProfilesCleanPathsArr) {
+Remove-Item -Path $Profile$Path -Recurse -Force -ErrorAction SilentlyContinue
+}
+}
+write-Host 'Temporary files and selected directories have been deleted successfully!' -ForegroundColor Green
+
+# очистка корзины
+$Recycler = (New-Object -ComObject Shell.Application).NameSpace(0xa)
+$Recycler.items() | foreach { rm $_.path -force -recurse }
+write-Host 'Cleaning the trash has been completed successfully!' -ForegroundColor Green
+
+# очистка браузера chrome
+$Items = @('Archived History',
+            'Cache\*',
+            'Cookies',
+            'History',
+            'Login Data',
+            'Top Sites',
+            'Visited Links',
+            'Web Data')
+$Folder = "$($env:LOCALAPPDATA)\Google\Chrome\User Data\Default"
+$Items | % { 
+    if (Test-Path "$Folder\$_") {
+        Remove-Item "$Folder\$_" 
+    }
+}
+write-Host 'Cleaning the Google Chrome browser has been completed successfully!' -ForegroundColor Green
+
+# очистка браузера edge
+rm C:\Users\*\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\*
+remove-item $env:localappdata\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\* -recurse -Force 2>$null
+Get-AppXPackage -AllUsers -Name Microsoft.MicrosoftEdge | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml" -Verbose}
+write-Host 'Cleaning the Edge browser has been completed successfully!' -ForegroundColor Green
+
+# установка CCleaner
+mkdir C:\CCleanerInstall
+Invoke-WebRequest -Uri "https://download.ccleaner.com/ccsetup578.exe" -OutFile C:\CCleanerInstall\ccsetup526.exe
+Start-Process -FilePath "c:\CCleanerInstall\ccsetup526.exe" -ArgumentList "/S"
+Start-Sleep -s 20
+rm -R -Force C:\CCleanerInstall
+write-Host 'CCleaner is installed successfully!' -ForegroundColor Green
+
+# запуск чистки при помощи CCleaner
+$path32 = Join-Path "$env:ProgramFiles(x86)" "Ccleaner"
+$path64 = Join-Path $env:ProgramFiles "Ccleaner"
+
+if(Test-Path $path32) {
+    $exePath = Join-Path $path32 "Ccleaner.exe"
+    & $exePath "/auto"
+} elseif (Test-Path $path64) {
+    $exePath = Join-Path $path64 "Ccleaner.exe"
+    & $exePath "/auto"
+}
