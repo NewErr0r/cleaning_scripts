d3a03fd3f8b156ca2a7cdbbdd90fad7ac5a41258
Dima <sechmma@gmail.com>
2022-11-02 23:26:53 +0300

    fixed

--- cleaning_scripts.ps1
+++ cleaning_scripts.ps1
@@ -45,23 +45,3 @@ rm C:\Users\*\AppData\Local\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\*
 remove-item $env:localappdata\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\* -recurse -Force 2>$null
 Get-AppXPackage -AllUsers -Name Microsoft.MicrosoftEdge | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml" -Verbose}
 write-Host 'Cleaning the Edge browser has been completed successfully!' -ForegroundColor Green
-
-# установка CCleaner
-mkdir C:\CCleanerInstall
-Invoke-WebRequest -Uri "https://download.ccleaner.com/ccsetup578.exe" -OutFile C:\CCleanerInstall\ccsetup526.exe
-Start-Process -FilePath "c:\CCleanerInstall\ccsetup526.exe" -ArgumentList "/S"
-Start-Sleep -s 20
-rm -R -Force C:\CCleanerInstall
-write-Host 'CCleaner is installed successfully!' -ForegroundColor Green
-
-# запуск чистки при помощи CCleaner
-$path32 = Join-Path "$env:ProgramFiles(x86)" "Ccleaner"
-$path64 = Join-Path $env:ProgramFiles "Ccleaner"
-
-if(Test-Path $path32) {
-    $exePath = Join-Path $path32 "Ccleaner.exe"
-    & $exePath "/auto"
-} elseif (Test-Path $path64) {
-    $exePath = Join-Path $path64 "Ccleaner.exe"
-    & $exePath "/auto"
-}
