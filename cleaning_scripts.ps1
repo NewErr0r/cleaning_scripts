bd6c3d4146bc4427eb31c01436187d22835b295b
Dima <96333513+NewErr0r@users.noreply.github.com>
2022-11-03 22:04:12 +0300

    Update cleaning_scripts.ps1

--- cleaning_scripts.ps1
+++ cleaning_scripts.ps1
@@ -4,7 +4,7 @@ $OtherCleanPathsArr = 'C:\Temp\*', 'C:\Windows\Temp\*', 'C:\swsetup'
 #системные пути для очистки
 $InProfilesCleanPathsArr = '\AppData\Local\Temp\*', '\AppData\Local\*.auc', '\AppData\Local\Microsoft\Terminal Server Client\Cache\*', '\AppData\Local\Microsoft\Windows\Temporary Internet Files\*', '\AppData\Local\Microsoft\Windows\WER\ReportQueue\*', '\AppData\Local\Microsoft\Windows\Explorer\*'
 
-#пути в профилях для очистки
+# пути в профилях для очистки
 $Profiles = Get-ChildItem (Get-ItemProperty -path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList').ProfilesDirectory -Exclude 'Administrator', 'Setup', 'Public', 'All Users', 'Default User'
 #извлекли из реестра местоположение профилей, сформировали список
 
